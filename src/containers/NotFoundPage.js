7625d41405ae597ed8314552ddca50b1fd4170f2
Farmin Farzin <farmin.f@gmail.com>
2017-12-21 08:45:18 +0100

    cleaning not found page

--- src/containers/NotFoundPage.js
+++ src/containers/NotFoundPage.js
@@ -2,20 +2,6 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import Accessibility from 'material-ui-icons/Accessibility';
 import {withStyles} from 'material-ui/styles';
-import {showLogoutButton} from '../actions/header';
-import {connect} from 'react-redux';
-
-const mapStateToProps = (state) => {
-  return {isLogoutButton: state.header.isLogoutButton}
-}
-
-const mapDispatchToProps = dispatch => {
-  return {
-    onShowHeader: isLogoutButton => {
-      dispatch(showLogoutButton({isLogoutButton: isLogoutButton}));
-    }
-  }
-}
 
 const styles = theme => ({
   icon: {
@@ -25,23 +11,18 @@ const styles = theme => ({
   }
 });
 
-class NotFoundPage extends React.Component {
+const NotFoundPage = (props) => {
 
-  componentWillMount() {
-    this.props.onShowHeader(false);
-  }
-
-  render() {
-    const {classes} = this.props;
-    return (
-      <div>
-        <Accessibility className={classes.icon}/>
-        <h1>Oooops</h1>
-        <h1>404 -
-          <Link to="/">Go home</Link>
-        </h1>
-      </div>
-    );
-  }
+  const {classes} = props;
+  return (
+    <div>
+      <Accessibility className={classes.icon}/>
+      <h1>Oooops</h1>
+      <h1>404 -
+        <Link to="/">Go home</Link>
+      </h1>
+    </div>
+  );
 }
-export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(NotFoundPage));
+
+export default(withStyles(styles)(NotFoundPage));
